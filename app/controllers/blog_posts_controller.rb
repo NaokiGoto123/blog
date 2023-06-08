2efd3bd986223215396693f03df259850cfef5e8
Naoki Goto <baron.naoki.g@gmail.com>
2023-06-08 11:09:21 +0900

    create show in controller

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,4 +2,9 @@ class BlogPostsController < ApplicationController
     def index
         @blog_posts = BlogPosts.all
     end
+
+    def show
+        @blog_post = BlogPosts.find(params[:id])
+    rescue
+    end
 end
